f6601dde275f23ad585a651caf4f0f767edbbfaa
Aleksandr Osipov <sashaosipov.98@gmail.com>
2020-12-18 18:09:43 +0300

    add test project

---
+++ CantorZassenhausTests/test.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+#include <string>
+//#include "Polynomial.h"
+#include "..\CantorZassenhaus\CantorZassenhaus.h"
+
+TEST(Polynomial, div1) {
+    Polynomial p1 = Polynomial({ 2, 1, 2, 2, 2, 0, 0, 2 });
+    Polynomial p2 = Polynomial({ 2, 0, 1, 1 });
+
+    Polynomial p3 = div(p1, p2, 3);
+
+    std::string result = "x^4+2x^3+2x^2+x+2";
+
+    EXPECT_EQ(p3.to_string(), result);
+    EXPECT_TRUE(true);
+}
